db267dab29198046ca976d6a36faa5b753a39f7d
Mctripp <mctripp94@gmail.com>
2020-03-10 17:42:30 -0400

    Add events.js
    Add events.js skeleton

--- assets/scripts/users/events.js
+++ assets/scripts/users/events.js
@@ -9,21 +9,46 @@ const getFormFields = require('./../../../lib/get-form-fields.js')
 const onGetGames = event => {
   event.preventDefault()
 
-  api.booksIndex()
+  api.getGames()
   // If succeed:
-    .then(ui.onBooksIndexSuccess)
+    .then(ui.onGetGamesSuccess)
   // If fail:
-    .catch(ui.onBooksIndexFailure)
+    .catch(ui.onGetGamesFailure)
 } //onGetGames
 
 const onNewGame = event => {
+  event.preventDefault()
 
+  api.newGame()
+  // If succeed:
+    .then(ui.onNewGameSuccess)
+  // If fail:
+    .catch(ui.onNewGameFailure)
 } //onNewGame
 
 const onFindGame = event => {
+  event.preventDefault()
 
+  api.findGame()
+  // If succeed:
+    .then(ui.onFindGameSuccess)
+  // If fail:
+    .catch(ui.onFindGameFailure)
 } //onFindGame
 
 const onUpdateGame = event => {
+  event.preventDefault()
 
+  api.updateGames()
+  // If succeed:
+    .then(ui.onUpdateGameSuccess)
+  // If fail:
+    .catch(ui.onUpdateGameFailure)
 } //onUpdateGame
+
+module.exports = {
+  onGetGames,
+  onNewGame,
+  onFindGame,
+  onUpdateGame
+}
