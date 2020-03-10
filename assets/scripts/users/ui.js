4b22dd63ccbb34bcd467678040fc06f0e6b0a005
Mctripp <mctripp94@gmail.com>
2020-03-10 17:36:46 -0400

    Add ui.js
    Add ui.js skeleton

---
+++ assets/scripts/users/ui.js
@@ -0,0 +1,49 @@
+'use strict'
+
+//Handle all UI changes:
+// SUCCESSES ------------------------
+
+onGetGamesSuccess = responseData => {
+
+} //onGetGamesSuccess
+
+onNewGameSuccess = responseData => {
+
+} //onNewGameSuccess
+
+onFindGameSuccess = responseData => {
+
+} //onFindGameSuccess
+
+onUpdateGameSuccess = responseData => {
+
+} //onUpdateGameSuccess
+
+//FAILURES --------------------------
+
+onGetGamesFailure = responseData => {
+
+} //onGetGamesFailure
+
+onNewGameFailure = responseData => {
+
+} //onNewGameFailure
+
+onFindGameFailure = responseData => {
+
+} //onFindGameFailure
+
+onUpdateGameFailure = responseData => {
+
+} //onUpdateGameFailure
+
+module.exports = {
+  onGetGamesSuccess,
+  onGetGamesFailure,
+  onNewGameSuccess,
+  onNewGameFailure,
+  onFindGameSuccess,
+  onFindGameFailure,
+  onUpdateGameSuccess,
+  onUpdateGameFailure
+}
