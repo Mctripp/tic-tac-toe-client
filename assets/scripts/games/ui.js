3c5a20a1e40163a9c7e2f9fef9dfdd69751aca4f
Mctripp <mctripp94@gmail.com>
2020-03-11 14:55:34 -0400

    Add pseudocode to games/ui.js

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -3,37 +3,37 @@
 // SUCCESSES ------------------------
 
 const onGetGamesSuccess = responseData => {
-
+  //Switch to game (?)
 } //onGetGamesSuccess
 
 const onNewGameSuccess = responseData => {
-
+  //Switch to new game (?)
 } //onNewGameSuccess
 
 const onFindGameSuccess = responseData => {
-
+  //Switch to found game (?)
 } //onFindGameSuccess
 
 const onUpdateGameSuccess = responseData => {
-
+  //End turn, remove interactivity from board
 } //onUpdateGameSuccess
 
 //FAILURES --------------------------
 
 const onGetGamesFailure = responseData => {
-
+  //Error msg
 } //onGetGamesFailure
 
 const onNewGameFailure = responseData => {
-
+  //Error msg
 } //onNewGameFailure
 
 const onFindGameFailure = responseData => {
-
+  //Error msg
 } //onFindGameFailure
 
 const onUpdateGameFailure = responseData => {
-
+  //Error msg
 } //onUpdateGameFailure
 
 module.exports = {
