8e4914d441d09f6b3eec37aa456eb32d54baaa31
Mctripp <mctripp94@gmail.com>
2020-03-11 16:36:35 -0400

    Add engine skeleton
    Add engine skeleton for ttt game

---
+++ assets/scripts/engine.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// determine which mark to use
+const getMarkType = () => {
+  
+} // getMarkType
+
+// mark clicked grid with player's mark, disable event on square
+const markGrid = () => {
+
+} //markGrid
+
+module.exports = {
+  markGrid
+}
