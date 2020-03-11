900de87680fbf889ad179415e7c75f6a82fac387
Mctripp <mctripp94@gmail.com>
2020-03-11 08:44:15 -0400

    Add users folder
    Add folder to hold .js files

---
+++ assets/scripts/users/events.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const ui = require('./ui.js')
+const api = require('./api.js')
+const getFormFields = require('./../../../lib/get-form-fields.js')
+
+//Handle all clicks, form submissions, etc.:
+
+const onGetGames = event => {
+  event.preventDefault()
+
+  api.getGames()
+  // If succeed:
+    .then(ui.onGetGamesSuccess)
+  // If fail:
+    .catch(ui.onGetGamesFailure)
+} //onGetGames
+
+const onNewGame = event => {
+  event.preventDefault()
+
+  api.newGame()
+  // If succeed:
+    .then(ui.onNewGameSuccess)
+  // If fail:
+    .catch(ui.onNewGameFailure)
+} //onNewGame
+
+const onFindGame = event => {
+  event.preventDefault()
+
+  api.findGame()
+  // If succeed:
+    .then(ui.onFindGameSuccess)
+  // If fail:
+    .catch(ui.onFindGameFailure)
+} //onFindGame
+
+const onUpdateGame = event => {
+  event.preventDefault()
+
+  api.updateGames()
+  // If succeed:
+    .then(ui.onUpdateGameSuccess)
+  // If fail:
+    .catch(ui.onUpdateGameFailure)
+} //onUpdateGame
+
+module.exports = {
+  onGetGames,
+  onNewGame,
+  onFindGame,
+  onUpdateGame
+}
