238de7fc2a4ba916f2a00b62d7c5bd62f326ef03
Mctripp <mctripp94@gmail.com>
2020-03-11 17:39:25 -0400

    Add box click event prototype
    Add box click -> fill with image for testing

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -2,12 +2,14 @@
 
 // determine which mark to use
 const getMarkType = () => {
-  
+
 } // getMarkType
 
 // mark clicked grid with player's mark, disable event on square
-const markGrid = () => {
-
+const markGrid = event => {
+  const boxId = event.target.id
+  $(`#${boxId}`).children().removeClass("hidden")
+  $(`#${boxId}`).children().attr("src", "./../../../public/images/O.jpg")
 } //markGrid
 
 module.exports = {
