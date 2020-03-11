c1f373a581a4caeb6f8642aa8aaae6a9c2e4af71
Mctripp <mctripp94@gmail.com>
2020-03-11 15:01:50 -0400

    Add requires, jQ to app.js
    Added dependencies and some jquery to HTML buttons in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,16 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+const userEvents = require("./users/events.js")
+const gameEvents = require("./games/events.js")
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+  $('.sign-in').on('submit', userEvents.onSignIn)
+  $('.sign-up').on('submit', userEvents.onSignUp)
+  $('.sign-out').on('submit', userEvents.onSignOut)
+  $('.change-password').on('submit', userEvents.onChangePassword)
 })
